5544be92dc1c34d42f9872a32c668284c748db34
Renee WalkerRobinson <rwr@Renees-MacBook-Air.local>
2021-08-22 21:14:13 -0400

    Refactored the Always 3 Script to be only three lines and use only one variable

--- always_three.rb
+++ always_three.rb
@@ -1,10 +1,4 @@
 puts "Give me a number"
 num = gets.to_i
-final_number = num
-final_number+=5
-final_number*=2
-final_number-=4
-final_number/=2
-final_number-=num
-puts "Always #{final_number}"
-end
+puts 'Always' + (((final_number+5) *2-4)/2 - num).to_i
+
